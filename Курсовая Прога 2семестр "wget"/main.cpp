536e3d219c0d53d1cf0169234eb4ebf3241c372a
apcel <apcel@yandex.ru>
2015-09-05 16:17:50 +0300

    autobuild by Sublime Text

--- Курсовая Прога 2семестр "wget"/main.cpp
+++ Курсовая Прога 2семестр "wget"/main.cpp
@@ -92,10 +92,10 @@ std::string gethost(char* address, int *begin, int *end) {
 	std::string s = address;
 	*end = s.size() / sizeof(char);
 	int temp = 0;
-	temp = s.find("\/\/", 0);
+	temp = s.find("//", 0);
 	if (temp != s.npos)
 		*begin = temp + 2;
-	temp = s.find("\/", *begin + 1);
+	temp = s.find("/", *begin + 1);
 	if (temp != s.npos)
 		*end = temp;
 #	ifdef DEBUG_GETHOST
