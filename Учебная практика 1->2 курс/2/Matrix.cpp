697e0677939865e25879dc6e43e3b322a90c7a3e
apcel <apcel@yandex.ru>
2015-09-07 01:29:10 +0300

    autobuild by Sublime Text

--- Учебная практика 1->2 курс/2/Matrix.cpp
+++ Учебная практика 1->2 курс/2/Matrix.cpp
@@ -20,24 +20,16 @@ MyMatrix<__Type> & MyMatrix<__Type>::operator + (const MyMatrix<__Type> &m)
 {
 
 }
-template<typename __Type>
-__Type & MyMatrix<__Type>::operator () (const unsigned long el)
-{
-	return *container[el / sizeN][el % sizeN];
-}
-template<typename __Type>
-__Type & MyMatrix<__Type>::operator () (const unsigned long elM, const unsigned long elN)
-{
-	return *container[elM][elN];
-}
+
+
 
 template<typename __Type>
 void MyMatrix<__Type>::transpone()
 {
 	for (int i = 0; i <= sizeN; ++i)
 		for (int j = i; j <= sizeM; ++j)
-			std::swap(container[i][j], container[j][i]);
-
+		//	std::swap(container[i][j], container[j][i]);
+			true;
 
 }
 
@@ -45,13 +37,13 @@ void MyMatrix<__Type>::transpone()
 
 /////////////////////Поиск////////////////////////////////
 template<typename __Type>
-int MyMatrix<__Type>::operator sizeRow()
+int MyMatrix<__Type>::sizeRow()
 {
 
 
 }
 template<typename __Type>
-int MyMatrix<__Type>::operator sizeCol()
+int MyMatrix<__Type>::sizeCol()
 {
 	return temp.size();
 
