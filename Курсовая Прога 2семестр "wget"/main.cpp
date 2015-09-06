6f849699c61482c21f9436527613f4dc41a0e06b
apcel <apcel@yandex.ru>
2015-09-06 18:05:14 +0300

    autobuild by Sublime Text

--- Курсовая Прога 2семестр "wget"/main.cpp
+++ Курсовая Прога 2семестр "wget"/main.cpp
@@ -210,7 +210,7 @@ int main (int argc, char* argv[])
     std::string server_reply;
     {
     temporaryInteger = recv(socketFd, server_reply_buf, 4, 0);
-	} while(server_reply.find("\r\n\r\n") != server_reply.npos)
+	} while(server_reply.find("\r\n\r\n") != server_reply.npos);
     log("received data: " + std::to_string(temporaryInteger));
     log(server_reply);
     //fprintf(localFd, "%s\n", server_reply);
