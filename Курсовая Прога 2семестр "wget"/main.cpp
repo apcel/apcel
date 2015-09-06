93ee41c738ec0f11200fcb6887a0815553d7bc60
apcel <apcel@yandex.ru>
2015-09-06 16:09:47 +0300

    autobuild by Sublime Text

--- Курсовая Прога 2семестр "wget"/main.cpp
+++ Курсовая Прога 2семестр "wget"/main.cpp
@@ -87,8 +87,9 @@ int main (int argc, char* argv[])
     "\nint     ai_socktype;			" + std::to_string(he->ai_family) +\
     "\nint     ai_protocol;			" + std::to_string(he->ai_protocol) +\
     "\nsize_t  ai_addrlen;			" + std::to_string(he->ai_addrlen) +\
-    "\nstruct  sockaddr *ai_addr;	" + std::to_string(he->ai_addr->sa_family) + "  " + std::string(he->ai_addr->sa_data) + \
-    "\nchar    *ai_canonname;     /* canonical name */	" + std::string(he->ai_canonname) + \
+    "\nstruct  sockaddr *ai_addr;	" + std::to_string(he->ai_addr->sa_family) + "  " + std::string(he->ai_addr->sa_data));
+
+    log("char    *ai_canonname;     /* canonical name */	" + std::string(he->ai_canonname) + \
     "\nstruct  addrinfo *ai_next; /* this struct can form a linked list */" + \
 "}");
 
