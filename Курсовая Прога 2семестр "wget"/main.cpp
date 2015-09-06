f8fa365c52911942af5a4cdf7cdaaa439ad43261
apcel <apcel@yandex.ru>
2015-09-06 17:42:29 +0300

    autobuild by Sublime Text

--- Курсовая Прога 2семестр "wget"/main.cpp
+++ Курсовая Прога 2семестр "wget"/main.cpp
@@ -22,7 +22,7 @@ struct requestSkeleton
 	std::string method = "GET";
 	char SP = char(32);
 	std::string httpVer = "HTTP/1.1";
-	std::string requestHeader = ""/*"(Accept: /*)"*/;
+	std::string requestHeader = "(Accept: */*)";
 	std::string CRLF = "\r\n\r\n";
 	int flags = 0;
 };
