cbacf3ca088b027924d8a0df5fc543a0e9751fee
apcel <apcel@yandex.ru>
2015-09-20 16:34:08 +0300

    autobuild by Sublime Text

--- Kursach_2Sem/main.cpp
+++ Kursach_2Sem/main.cpp
@@ -295,7 +295,7 @@ int main (int argc, char* argv[])
 
 void show_help(char* cmdname) {
     fprintf(stdout, "A simple wget implementation\n");
-    fprintf(stdout, "Usage: %s [-id][-o FILENAME][--ignore-all] ADDRESS\n", cmdname);
+    fprintf(stdout, "Usage:\t%s [-id][-o FILENAME][--ignore-all] ADDRESS\n", cmdname);
     fprintf(stdout, "\t%s [-h][--help]\n", cmdname);
 
     fprintf(stdout, "\t\t-h  --help\n\t show this help message\n");
