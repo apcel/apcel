fdfd7ecf2944be958e85954184a0aa53cf8bd05d
apcel <apcel@yandex.ru>
2015-11-11 07:40:53 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <boost/asio.hpp>
+//#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
 int main() {
     std::cout << "Hello world! - boost" << std::endl;
