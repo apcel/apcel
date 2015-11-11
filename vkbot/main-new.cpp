3b4b39f41930adf9465406d5bc92bd57fd0502ef
apcel <apcel@yandex.ru>
2015-11-11 23:53:57 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -1,32 +1,51 @@
-#include <iostream>
-//#include <boost/asio.hpp>
+// Copyright Vladimir Prus 2002-2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/* The simplest usage of the library.
+ */
+
 #include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <iostream>
+#include <iterator>
 using namespace std;
-int main(int ac, char* av[]) {
-//     namespace po = boost::program_options;
-// // Declare the supported options.
-//     po::options_description desc("Allowed options");
-//     desc.add_options()
-//     ("help", "produce help message")
-//     ("compression", po::value<int>(), "set compression level")
-//     ;
 
-//     po::variables_map vm;
-//     po::store(po::parse_command_line(ac, av, desc), vm);
-//     po::notify(vm);
+int main(int ac, char* av[])
+{
+    try {
 
-//     if (vm.count("help")) {
-//         cout << desc << "\n";
-//         return 1;
-//     }
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("compression", po::value<double>(), "set compression level")
+        ;
 
-//     if (vm.count("compression")) {
-//         cout << "Compression level was set to "
-//              << vm["compression"].as<int>() << ".\n";
-//     } else {
-//         cout << "Compression level was not set.\n";
-//     }
+        po::variables_map vm;
+        po::store(po::parse_command_line(ac, av, desc), vm);
+        po::notify(vm);
 
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("compression")) {
+            cout << "Compression level was set to "
+                 << vm["compression"].as<double>() << ".\n";
+        } else {
+            cout << "Compression level was not set.\n";
+        }
+    }
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
 
     return 0;
-};
+}
---
+++ vkbot/main-new_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+//#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+using namespace std;
+int main(int ac, char* av[]) {
+    namespace po = boost::program_options;
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+//     desc.add_options()
+//     ("help", "produce help message")
+//     ("compression", po::value<int>(), "set compression level")
+//     ;
+
+//     po::variables_map vm;
+//     po::store(po::parse_command_line(ac, av, desc), vm);
+//     po::notify(vm);
+
+//     if (vm.count("help")) {
+//         cout << desc << "\n";
+//         return 1;
+//     }
+
+//     if (vm.count("compression")) {
+//         cout << "Compression level was set to "
+//              << vm["compression"].as<int>() << ".\n";
+//     } else {
+//         cout << "Compression level was not set.\n";
+//     }
+
+
+    return 0;
+};
