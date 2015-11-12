0d477f698cc3489b752af22331978eba7359f15c
apcel <apcel@yandex.ru>
2015-11-12 04:09:14 +0300

    Autobuild (make) by Sublime Text

--- vkbot/https.cpp
+++ vkbot/https.cpp
@@ -1,22 +1,6 @@
 #include "https.h"
 https::https() {
 
+
 };
 
-struct https::linkStruct
-{
-    std::string protocol = "";
-    std::string hostname = "";
-    std::string relative = "";
-    std::string filename = "";
-};
-
-struct https::requestSkeleton
-{
-    std::string method = "GET";
-    char SP = char(32);
-    std::string httpVer = "HTTP/1.1";
-    std::string requestHeader = "\r\nAccept: */*\r\nAccept-Charset: utf-8\r\nConnection: Keep-Alive\r\n";
-    std::string CRLF = "\r\n\r\n";
-    int flags = 0;
-};
--- vkbot/https.h
+++ vkbot/https.h
@@ -7,9 +7,23 @@ public:
     https();
     ~https();
 private:
+    struct requestSkeleton{
+        std::string method = "GET";
+        char SP = char(32);
+        std::string httpVer = "HTTP/1.1";
+        std::string requestHeader = "\r\nAccept: */*\r\nAccept-Charset: utf-8\r\nConnection: Keep-Alive\r\n";
+        std::string CRLF = "\r\n\r\n";
+        int flags = 0;
+    };
+    struct linkStruct{
+        std::string protocol = "";
+        std::string hostname = "";
+        std::string relative = "";
+        std::string filename = "";
+    };
     struct addrinfo *he;
-    struct requestSkeleton;
-    struct linkStruct;
+    requestSkeleton requestSkeletonEntity;
+    linkStruct linkStructEntity;
     int socketFd;
     int tempInt;
 };
