5b0b2829ccf8b02958ca00be52e4f59a4b812ec8
apcel <apcel@yandex.ru>
2015-11-12 06:32:22 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main.cpp
+++ vkbot/main.cpp
@@ -13,7 +13,7 @@ int main(int ac, char* av[])
 {
     https * httpsObject = new https;
     std::string method, parameters, token;
-    // httpsObject->request("https://api.vk.com/method/" + method + "?" + parameters + "&access_token=" + token);
+    httpsObject->request("https://api.vk.com/method/" + method + "?" + parameters + "&access_token=" + token);
     
     vkapi * vkapiObject = new vkapi;
     vkapiObject->groupsGetById("1");
