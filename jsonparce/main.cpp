8f9acb4dc2d707517a17303f61e1da5e5cda25f9
apcel <apcel@yandex.ru>
2015-11-12 10:54:53 +0300

    Отдельный парсер json Получает размер массива

---
+++ jsonparce/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    std::string jsonString;
+    if (FILE *fp = fopen("filename", "r"))
+    {
+        char buf[1024];
+        while (size_t len = fread(buf, 1, sizeof(buf), fp))
+            jsonString.insert(jsonString.end(), buf, buf + len);
+        fclose(fp);
+    }
+    int findInt;
+    std::string responseString = "\"response\":";
+    findInt = jsonString.find(responseString);
+    std::string tempString;
+    if (findInt != jsonString.npos) {
+        jsonString = jsonString.substr(findInt + responseString.size(), jsonString.npos);
+        // std::cout << jsonString;
+        if (jsonString.front() == '[') {
+            jsonString = jsonString.substr(1, jsonString.npos);
+            while (jsonString.front() != ',') {
+                // fprintf(stderr, "%s\n", "asdf");
+
+                // std::cout << jsonString.front();
+                tempString.push_back(jsonString.front());
+                jsonString = jsonString.substr(1, jsonString.npos);
+            };
+        } else if (jsonString.front() == '{') {fprintf(stderr, "%s\n", "ШАЙТАНАМА!!!"); return -100;}
+        int numberOfEntities;
+        numberOfEntities = stoi(tempString, 0, 10);
+        std::cout << numberOfEntities;
+    }
+    return 0;
+}
