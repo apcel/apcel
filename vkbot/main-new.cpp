d1c6e5c47cee7b6571b82f87833123d228240e37
apcel <apcel@yandex.ru>
2015-11-12 03:23:16 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -3,26 +3,12 @@
 
 #include <iostream>
 #include <iterator>
-#include <glib.h>
+#include <fstream>
+// #include <glib.h>
 
 int main(int ac, char* av[])
 {
-    GKeyFile * my_key_file = g_key_file_new();
-    GError * error;
-    GKeyFileFlags gkff;
-    const gchar * configFileName = "../example-config.ini";
-    my_key_file=g_key_file_ref(my_key_file);
-    g_key_file_load_from_file(my_key_file,
-        configFileName,
-        gkff,
-        &error);
-    //std::cout << g_key_file_to_data (my_key_file, NULL, NULL) << std::endl;
-
-
-    gchar ** my_keys = g_key_file_get_keys(my_key_file, g_key_file_get_start_group (my_key_file), NULL, NULL);
-
-    fprintf(stdout, "%s\n", *(my_keys + 1));
-
+    std::ifstream tokenfile;
 
     return 0;
 }
