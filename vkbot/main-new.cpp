f116b445b614e5cb62d7e7761dd87253e9e1e0e9
apcel <apcel@yandex.ru>
2015-11-12 03:18:54 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -21,7 +21,7 @@ int main(int ac, char* av[])
 
     gchar ** my_keys = g_key_file_get_keys(my_key_file, g_key_file_get_start_group (my_key_file), NULL, NULL);
 
-    std::cout << my_keys << std::endl;
+    std::cout << *my_keys << std::endl;
 
     return 0;
 }
