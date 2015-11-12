75cdd0d73f0d9f3fb14ef1f0062f72d522b6a4cc
apcel <apcel@yandex.ru>
2015-11-12 05:24:36 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -28,8 +28,8 @@ int main(int ac, char* av[])
 
     httpsObject->request("https://api.vk.com/method/messages.send?domain=apcel&message=%22test%20from%20api%202%22&access_token=98a9164bfdc63abc1752bccfd0765d4d34e1b72159a5faf4d3577184db9fef517e38d440a1fc80c8e8148");
     httpsObject->getResponse();
+
+
     httpsObject->~https();
-
-
     ; return 0;
 }
---
+++ vkbot/vkapi.h
@@ -0,0 +1,7 @@
+class vkapi
+{
+public:
+    vkapi();
+    ~vkapi();
+    
+};
