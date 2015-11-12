8bd58dc0bb52901ed68a5defe12012ea3dc2aa12
apcel <apcel@yandex.ru>
2015-11-12 03:00:35 +0300

    Autobuild (make) by Sublime Text

--- vkbot/main-new.cpp
+++ vkbot/main-new.cpp
@@ -1,11 +1,3 @@
-// Copyright Vladimir Prus 2002-2004.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt
-// or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-/* The simplest usage of the library.
- */
-
 //#include <boost/program_options.hpp>
 //namespace po = boost::program_options;
 
@@ -15,6 +7,17 @@
 
 int main(int ac, char* av[])
 {
-   
+    GKeyFile * my_key_file = g_key_file_new();
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
