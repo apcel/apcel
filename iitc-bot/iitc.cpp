eecf85373068cf360189f9ce5f3f128f225b82e4
apcel <apcel@yandex.ru>
2016-01-30 05:00:57 +0300

    Added support for json settings storing

--- iitc-bot/iitc.cpp
+++ iitc-bot/iitc.cpp
@@ -1,6 +1,7 @@
 #include "iitc.h"
-iitc::iitc() {
-
+iitc::iitc(std::string SACSID, std::string headerCSRF, std::string cookieCSRF) {
+    setCookieSACSID(SACSID);
+    setCSRF(cookieCSRF, headerCSRF);
 }
 iitc::~iitc() {
 
@@ -11,30 +12,30 @@ rapidjson::Document iitc::request(std::string method, std::string params) {
 
     auto response = cpr::Post(
                         cpr::Url
-                        {
-                            iitcBaseURL + method
-                        },
+    {
+        iitcBaseURL + method
+    },
 
-                        cpr::Header
-                        {
-                            {
-                                "Cookie", cookieSACSID + cookieCSRF
-                            },
-                            {
-                                "Accept", "application/json"
-                            },
-                            {
-                                "Referer", "https://www.ingress.com/intel"
-                            },
-                            {
-                                "X-CSRFToken", headerCSRF
-                            }
-                        },
+    cpr::Header
+    {
+        {
+            "Cookie", cookieSACSID + cookieCSRF
+        },
+        {
+            "Accept", "application/json"
+        },
+        {
+            "Referer", "https://www.ingress.com/intel"
+        },
+        {
+            "X-CSRFToken", headerCSRF
+        }
+    },
 
-                        cpr::Body
-                        {
-                            "{\"guid\":\"fa0838e5401643b6ac6b58b981f3994f.16\",\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}"
-                        }
+    cpr::Body
+    {
+        "{\"guid\":\"fa0838e5401643b6ac6b58b981f3994f.16\",\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}"
+    }
                     );
 
     std::cout << response.text << std::endl << std::endl;
@@ -42,10 +43,21 @@ rapidjson::Document iitc::request(std::string method, std::string params) {
 
     rapidjson::Document jsonResponse;
     jsonResponse.Parse(response.text.c_str());
-    if(jsonResponse.HasParseError()) {
-            std::cout << response.text << std::endl;
-            assert(false);
+    if (jsonResponse.HasParseError()) {
+        std::cout << response.text << std::endl;
+        assert(false);
     };
     return jsonResponse;
 
 }
+
+void iitc::setCookieSACSID(std::string SACSID) {
+    assert(!SACSID.empty());
+    cookieSACSID = SACSID;
+};
+void iitc::setCSRF(std::string cookie, std::string header) {
+    assert(!cookie.empty());
+    assert(!header.empty());
+    headerCSRF = header;
+    cookieCSRF = cookie;
+};
--- iitc-bot/main.cpp
+++ iitc-bot/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <cpr/cpr.h>
-
+#include <fstream>
+#include <iostream>
 
 #include "rapidjson/document.h"
 #include "iitc.h"
 // #include "rapidjson/stringbuffer.h"
 
-int main(int argc, char** argv) {
+rapidjson::Document loadSettings(std::string fileName) {
+    std::ifstream jsonSettingsFile(fileName);
+    std::string jsonSettings((std::istreambuf_iterator<char>(jsonSettingsFile)),
+                             std::istreambuf_iterator<char>());
+    rapidjson::Document d;
+    d.Parse(jsonSettings.c_str());
+    if ( !d.IsObject()) {
+        std::cerr << "Troubles with parsing json settings in '" + fileName + "'"  << std::endl
+                  << "Please ensure it is a correct json object" << std::endl;
+        exit(1);
+    };
+    return d;
+}
 
-    iitc iitc;
+
+
+int main(int argc, char** argv) {
+    rapidjson::Document settings = loadSettings("jsonSettingsFile.json");
+    iitc iitc(settings["cookieSACSID"].GetString(), settings["headerCSRF"].GetString(), settings["cookieCSRF"].GetString());
     rapidjson::Document response = iitc.request("getPortalDetails", "");
     std::cout << response["result"][1].GetString() << std::endl;
     // std::cout << response.text << std::endl;
