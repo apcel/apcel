8f6c409ec8dae04c4855073f06321dc6e02bb2bd
apcel <apcel@yandex.ru>
2016-01-31 16:35:44 +0300

    Updated main.cpp Some bad code to iterate in received from iitc map object

--- iitc-bot/main.cpp
+++ iitc-bot/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 
+#define RAPIDJSON_NOMEMBERITERATORCLASS
 #include "rapidjson/document.h"
 #include "iitc.h"
 #include "portal.h"
@@ -30,13 +31,49 @@ int main(int argc, char** argv) {
     // rapidjson::Document response = iitc.request("getPortalDetails", "");
     // std::cout << response["result"][1].GetString() << std::endl;
     // std::cout << response.text << std::endl;
-    portal portal;
-    rapidjson::Document portalJSON1 = parceJSONFromFile("out.json");
-    rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
+    // portal portal;
+    rapidjson::Document territoryJSON = parceJSONFromFile("territory.json");
+    // rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
 
     // if(!portal.parseJSON(&portalJSON))
     //     std::cout << "Error while parsing" << std::endl;
-    if(portal.parseJSON(&portalJSON1)){};
-    if(portal.parseJSON(&portalJSON2)){};
-    return 0;
+    // if(portal.parseJSON(&portalJSON1)){};
+
+
+    std::vector<std::vector<portal *>*> *map = new std::vector<std::vector<portal *>*> ;
+    std::vector<portal *> *chunk = new std::vector<portal *>;
+    portal * entity = new portal();
+    rapidjson::Value & mapJSON = territoryJSON["result"]["map"];
+    for (rapidjson::Value::MemberIterator chunkJSON = mapJSON.MemberBegin();
+            chunkJSON != mapJSON.MemberEnd(); ++chunkJSON) {
+        chunk->clear();
+        rapidjson::Value & temp = chunkJSON->value;
+        std::string asdf = chunkJSON->name.GetString();
+        for (rapidjson::Value::MemberIterator entityJSON = temp.MemberBegin();
+                entityJSON != temp.MemberEnd(); ++entityJSON) {
+            rapidjson::Value  & entities = entityJSON->value;
+            for (int i = 0; i < entities.Size(); i++) {
+                rapidjson::Value & tempValue = entities[i];
+                if (entity->parseJSON( & tempValue )) {
+                    chunk->push_back(entity);
+                    entity = new portal();
+                };
+            };
+        };
+        std::cout << "#####################################################" << std::endl;
+        if (!chunk->empty()) {
+            std::cout << chunk->size() << "\tportals in " << asdf << std::endl;
+            map->push_back(chunk);
+            std::cout << "Portals: " << std::endl;
+            for(auto i : *chunk) {
+                i->printData();
+                std::cout << "===================================================" << std::endl;
+            }
+        } else {
+            std::cerr << "No\tportals in " << asdf << std::endl;
+        }
+    };
+    // for (auto chunks : *map)
+
+        return 0;
 }
