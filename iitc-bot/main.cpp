ca9cb15ffeac5eec224442b3650e2e5d12eee304
apcel <apcel@yandex.ru>
2016-02-02 05:39:18 +0300

    Updated main.cpp Trying to get something human-readable from new "iitc class" functions

--- iitc-bot/main.cpp
+++ iitc-bot/main.cpp
@@ -26,54 +26,68 @@ rapidjson::Document parceJSONFromFile(std::string fileName) {
 
 
 int main(int argc, char** argv) {
-    // rapidjson::Document settings = parceJSONFromFile("jsonSettingsFile.json");
-    // iitc iitc(settings["cookieSACSID"].GetString(), settings["headerCSRF"].GetString(), settings["cookieCSRF"].GetString());
+    rapidjson::Document settings = parceJSONFromFile("jsonSettingsFile.json");
+    iitc iitc(settings["cookieSACSID"].GetString(), settings["headerCSRF"].GetString(), settings["cookieCSRF"].GetString());
     // rapidjson::Document response = iitc.request("getPortalDetails", "");
     // std::cout << response["result"][1].GetString() << std::endl;
     // std::cout << response.text << std::endl;
     // portal portal;
-    rapidjson::Document territoryJSON = parceJSONFromFile("territory.json");
-    // rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
+    /*    rapidjson::Document territoryJSON = parceJSONFromFile("territory.json");
+        // rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
 
-    // if(!portal.parseJSON(&portalJSON))
-    //     std::cout << "Error while parsing" << std::endl;
-    // if(portal.parseJSON(&portalJSON1)){};
+        // if(!portal.parseJSON(&portalJSON))
+        //     std::cout << "Error while parsing" << std::endl;
+        // if(portal.parseJSON(&portalJSON1)){};
 
 
-    std::vector<std::vector<portal *>*> *map = new std::vector<std::vector<portal *>*> ;
-    std::vector<portal *> *chunk = new std::vector<portal *>;
-    portal * entity = new portal();
-    rapidjson::Value & mapJSON = territoryJSON["result"]["map"];
-    for (rapidjson::Value::MemberIterator chunkJSON = mapJSON.MemberBegin();
-            chunkJSON != mapJSON.MemberEnd(); ++chunkJSON) {
-        chunk->clear();
-        rapidjson::Value & temp = chunkJSON->value;
-        std::string asdf = chunkJSON->name.GetString();
-        for (rapidjson::Value::MemberIterator entityJSON = temp.MemberBegin();
-                entityJSON != temp.MemberEnd(); ++entityJSON) {
-            rapidjson::Value  & entities = entityJSON->value;
-            for (int i = 0; i < entities.Size(); i++) {
-                rapidjson::Value & tempValue = entities[i];
-                if (entity->parseJSON( & tempValue )) {
-                    chunk->push_back(entity);
-                    entity = new portal();
+        std::vector<std::vector<portal *>*> *map = new std::vector<std::vector<portal *>*> ;
+        std::vector<portal *> *chunk = new std::vector<portal *>;
+        portal * entity = new portal();
+        rapidjson::Value & mapJSON = territoryJSON["result"]["map"];
+        for (rapidjson::Value::MemberIterator chunkJSON = mapJSON.MemberBegin();
+                chunkJSON != mapJSON.MemberEnd(); ++chunkJSON) {
+            chunk->clear();
+            rapidjson::Value & temp = chunkJSON->value;
+            std::string asdf = chunkJSON->name.GetString();
+            for (rapidjson::Value::MemberIterator entityJSON = temp.MemberBegin();
+                    entityJSON != temp.MemberEnd(); ++entityJSON) {
+                rapidjson::Value  & entities = entityJSON->value;
+                for (int i = 0; i < entities.Size(); i++) {
+                    rapidjson::Value & tempValue = entities[i];
+                    if (entity->parseJSON( & tempValue )) {
+                        chunk->push_back(entity);
+                        entity = new portal();
+                    };
                 };
             };
-        };
-        std::cout << "#####################################################" << std::endl;
-        if (!chunk->empty()) {
-            std::cout << chunk->size() << "\tportals in " << asdf << std::endl;
-            map->push_back(chunk);
-            std::cout << "Portals: " << std::endl;
-            for(auto i : *chunk) {
-                i->printData();
-                std::cout << "===================================================" << std::endl;
+            std::cout << "#####################################################" << std::endl;
+            if (!chunk->empty()) {
+                std::cout << chunk->size() << "\tportals in " << asdf << std::endl;
+                map->push_back(chunk);
+                std::cout << "Portals: " << std::endl;
+                for(auto i : *chunk) {
+                    i->printData();
+                    std::cout << "===================================================" << std::endl;
+                }
+            } else {
+                std::cerr << "No\tportals in " << asdf << std::endl;
             }
-        } else {
-            std::cerr << "No\tportals in " << asdf << std::endl;
-        }
-    };
-    // for (auto chunks : *map)
+        };
+        for(auto chunks: * map){
+            for(auto entities: *chunks)
+                delete entities;
+            delete chunks;
+        }*/
 
-        return 0;
+    // for (auto chunks : *map)
+    short zoom = 15;
+    std::cout <<
+              // iitc.pointToTileId(
+                  iitc.lngToTile(37810107, iitc.TILES_PER_EDGE[zoom])
+                  // iitc.latToTile(long(55783155), iitc.TILES_PER_EDGE[zoom]),
+                  // zoom,
+                  // 0
+              // )
+              << std::endl;
+    return 0;
 }
