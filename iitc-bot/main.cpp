b5ea91f7abacfcd459f2c2b7a52dd3b54a457f0b
apcel <apcel@yandex.ru>
2016-03-04 06:48:00 +0300

    Updated main.cpp Now testing new class (while others do not work :<)

--- iitc-bot/main.cpp
+++ iitc-bot/main.cpp
@@ -8,6 +8,7 @@
 #include "rapidjson/prettywriter.h"
 #include "iitc.h"
 #include "portal.h"
+#include "multiplePortals.h"
 // #include "rapidjson/stringbuffer.h"
 
 rapidjson::Document parceJSONFromFile(std::string fileName) {
@@ -53,84 +54,96 @@ int main(int argc, char** argv) {
 
 
     // rapidjson::Document portalJson = parceJSONFromFile("portal.json");
-    rapidjson::Document fullPortalJson = parceJSONFromFile("jsons/out.json");
-    portal portalObject(fullPortalJson["result"]);
-    portalObject.printData();
-    std::cout << std::endl;
 
+    // rapidjson::Document fullPortalJson = parceJSONFromFile("jsons/out.json");
+    // portal portalObject(fullPortalJson["result"]);
+    // portalObject.printData();
+    // std::cout << std::endl;
+    std::string drawTools = "[{\"type\":\"polygon\",\"latLngs\":[{\"lat\":55.76592736299077,\"lng\":37.78829097747803},{\"lat\":55.752026,\"lng\":37.78283},{\"lat\":55.74972397348254,\"lng\":37.845325469970696},{\"lat\":55.77140746269181,\"lng\":37.83519744873047}],\"color\":\"#a24ac3\"}]";
+    multiplePortals territory;
+    if (territory.acceptIITCPolygon(drawTools)) {
+        std::cout << "IT WORKS!!\n";
+        territory.process();
+        // territory.highestLevel(7);
+        // territory.atLevel(8);
+        // territory.wtf();
+        std::cout << "IT WORKS!!\n";
 
+    } else {
+        std::cerr << "Nope.\n";
+    }
     exit(0);
 }
 #ifdef RANDOM_STRING_NOT_DEFINED
 
-    short zoom = 15;
-    std::string titlekeys =
-        iitc.pointToTileId(
-            iitc.lngToTile(37829358, iitc.TILES_PER_EDGE[zoom]),
-            iitc.latToTile(55775830, iitc.TILES_PER_EDGE[zoom]),
-            zoom,
-            0
-        );
-    std::cout << titlekeys << std::endl;
-    std::string params = "{\"tileKeys\": [\"" + titlekeys + "\"],\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}";
-    // "{\"tileKeys\":[\"15_19362_10000_0_8_100\",\"15_19362_9999_0_8_100\",\"15_19363_10000_0_8_100\",\"15_19363_9999_0_8_100\",\"15_19361_10000_0_8_100\",\"15_19361_9999_0_8_100\"],\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}";
-    // std::cout << params << std::endl;
-    // rapidjson::Document response = iitc.request("getEntities", params);
-    // std::cout << response["result"][1].GetString() << std::endl;
-    // std::cout << response.text << std::endl;
-    // portal portal;
-    rapidjson::Document territoryJSON = parceJSONFromFile("file.txt");
-    // rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
-    // rapidjson::Document & territoryJSON = response;
-    // if(!portal.parseJSON(&portalJSON))
-    //     std::cout << "Error while parsing" << std::endl;
-    // if(portal.parseJSON(&portalJSON1)){};
+short zoom = 15;
+std::string titlekeys =
+    iitc.pointToTileId(
+        iitc.lngToTile(37829358, iitc.TILES_PER_EDGE[zoom]),
+        iitc.latToTile(55775830, iitc.TILES_PER_EDGE[zoom]),
+        zoom,
+        0
+    );
+std::cout << titlekeys << std::endl;
+std::string params = "{\"tileKeys\": [\"" + titlekeys + "\"],\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}";
+// "{\"tileKeys\":[\"15_19362_10000_0_8_100\",\"15_19362_9999_0_8_100\",\"15_19363_10000_0_8_100\",\"15_19363_9999_0_8_100\",\"15_19361_10000_0_8_100\",\"15_19361_9999_0_8_100\"],\"v\":\"9ffe0cfdf367c491a20802d3c606d679992e8b08\"}";
+// std::cout << params << std::endl;
+// rapidjson::Document response = iitc.request("getEntities", params);
+// std::cout << response["result"][1].GetString() << std::endl;
+// std::cout << response.text << std::endl;
+// portal portal;
+rapidjson::Document territoryJSON = parceJSONFromFile("file.txt");
+// rapidjson::Document portalJSON2 = parceJSONFromFile("portal.json");
+// rapidjson::Document & territoryJSON = response;
+// if(!portal.parseJSON(&portalJSON))
+//     std::cout << "Error while parsing" << std::endl;
+// if(portal.parseJSON(&portalJSON1)){};
 
 
-    std::vector<std::vector<portal *>*> *map = new std::vector<std::vector<portal *>*> ;
-    std::vector<portal *> *chunk = new std::vector<portal *>;
-    portal * entity = new portal();
-    rapidjson::Value & mapJSON = territoryJSON["result"]["map"];
-    for (rapidjson::Value::MemberIterator chunkJSON = mapJSON.MemberBegin();
-            chunkJSON != mapJSON.MemberEnd(); ++chunkJSON) {
-        chunk->clear();
-        rapidjson::Value & temp = chunkJSON->value;
-        std::string asdf = chunkJSON->name.GetString();
-        for (rapidjson::Value::MemberIterator entityJSON = temp.MemberBegin();
-                entityJSON != temp.MemberEnd(); ++entityJSON) {
-            rapidjson::Value  & entities = entityJSON->value;
-            for (int i = 0; i < entities.Size(); i++) {
-                rapidjson::Value & tempValue = entities[i];
-                // if (entity->parseJSON( & tempValue )) {
-                chunk->push_back(entity);
-                entity = new portal();
-                // };
-            };
+std::vector<std::vector<portal *>*> *map = new std::vector<std::vector<portal *>*> ;
+std::vector<portal *> *chunk = new std::vector<portal *>;
+portal * entity = new portal();
+rapidjson::Value & mapJSON = territoryJSON["result"]["map"];
+for (rapidjson::Value::MemberIterator chunkJSON = mapJSON.MemberBegin();
+        chunkJSON != mapJSON.MemberEnd(); ++chunkJSON) {
+    chunk->clear();
+    rapidjson::Value & temp = chunkJSON->value;
+    std::string asdf = chunkJSON->name.GetString();
+    for (rapidjson::Value::MemberIterator entityJSON = temp.MemberBegin();
+            entityJSON != temp.MemberEnd(); ++entityJSON) {
+        rapidjson::Value  & entities = entityJSON->value;
+        for (int i = 0; i < entities.Size(); i++) {
+            rapidjson::Value & tempValue = entities[i];
+            // if (entity->parseJSON( & tempValue )) {
+            chunk->push_back(entity);
+            entity = new portal();
+            // };
         };
-        std::cout << "#####################################################" << std::endl;
-        if (!chunk->empty()) {
-            std::cout << chunk->size() << "\tportals in " << asdf << std::endl;
-            map->push_back(chunk);
-            std::cout << "Portals: " << std::endl;
-            for (auto i : *chunk) {
-                i->printData();
-                std::cout << "===================================================" << std::endl;
-            }
-        } else {
-            std::cerr << "No\tportals in " << asdf << std::endl;
+    };
+    std::cout << "#####################################################" << std::endl;
+    if (!chunk->empty()) {
+        std::cout << chunk->size() << "\tportals in " << asdf << std::endl;
+        map->push_back(chunk);
+        std::cout << "Portals: " << std::endl;
+        for (auto i : *chunk) {
+            i->printData();
+            std::cout << "===================================================" << std::endl;
         }
-    };
-    for (auto chunks : * map) {
-        for (auto entities : *chunks)
-            delete entities;
-        delete chunks;
-    };
-    rapidjson::Document doc;
-    doc.SetObject();
-    rapidjson::Document::AllocatorType& allocator = doc.GetAllocator();
-    doc.AddMember("map", mapJSON, allocator);
-    saveJSONToFile(&doc, "file.txt");
-    // for (auto chunks : *map)
-    return 0;
+    } else {
+        std::cerr << "No\tportals in " << asdf << std::endl;
+    }
+};
+for (auto chunks : * map) {
+    for (auto entities : *chunks)
+        delete entities;
+    delete chunks;
+};
+rapidjson::Document doc;
+doc.SetObject();
+rapidjson::Document::AllocatorType& allocator = doc.GetAllocator();
+doc.AddMember("map", mapJSON, allocator);
+saveJSONToFile(&doc, "file.txt");
+// for (auto chunks : *map)
+return 0;
 }
 #endif
